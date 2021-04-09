081b7cad98e4bc449dda813a1ba1121696de550e
Alicia Schonefeld <schonefeld.dev@gmail.com>
2021-04-09 15:02:33 +0200

    update: api route for auth

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', authRouter);
+app.use('/api/v1/', authRouter);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/question', questionRouter);
 app.use('/api/v1/category', categoryRouter);
